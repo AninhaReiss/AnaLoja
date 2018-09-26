383f117e6fe7d3a2c1cda817c8c6cf5c57d55a57
ana-pc <sreis.aninha@gmail.com>
2018-09-26 17:38:02 -0400

    Criado teste unitario

---
+++ AnaLoja.Tests/ProdutoTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnaLoja.Data;
+using AnaLoja.Data.Entities;
+
+namespace AnaLoja.Tests
+{
+    [TestClass]
+    public class ProdutoTests
+    {
+        // [metodo]_[condicao]_[resultadoEsperado]
+        [TestMethod]
+        public void Descricao_ObterValor_DescricaoPreenchida()
+        {
+            //Arrange
+            Produto produto = new Produto();
+            produto.Descricao = "HD externo 1TB";
+            string resultadoEsperado = "HD externo 1TB";
+
+            //Act
+            string resultadoAtual = produto.Descricao;
+
+            //Assert
+            Assert.AreEqual(resultadoEsperado, resultadoAtual);
+        }
+
+
+    }
+}
